7e58eb2adeb9d4b1aba85a9a1684e90f021fc4a9
Ibrahim aljohany <sseret57@gmail.com>
2023-08-29 11:42:40 +0300

    final sign up page (maybe)

--- signUp.js
+++ signUp.js
@@ -1,30 +1,67 @@
 // Check if the password contains at least 1 capital letter, 1 small letter, 1 number, and 1 special character.
 
-function checkPassword(password) {
-    if(password.length>8)
-    document.getElementById("length").classList.toggle("correct");
+function checkPassword(password, passCofirm) {
+  let numberOfConditions = 6;
   // Check if the password contains at least 1 capital letter.
-  if (/[A-Z]/.test(password)) {
-    document.getElementById("cap").classList.toggle("correct");
+  if (/[A-Z]/.test(password))
+    document.getElementById("cap").style.color = "green";
+  else {
+    document.getElementById("cap").style.color = "grey";
+    numberOfConditions--;
   }
-  
-  // Check if the password contains at least 1 small letter.
- if(/[a-z]/.test(password))
- document.getElementById("small").classList.toggle("correct");
-  // Check if the password contains at least 1 number.
-  if(/[0-9]/.test(password))
-  document.getElementById("number").classList.toggle("correct");
-  // Check if the password contains at least 1 special character.
-  if(/[!@#$%^&*()_+-]/.test(password))
-  document.getElementById("special").classList.toggle("correct");
-  
-}
-// Add an event listener to the password input field
-const passwordInput = document.getElementById("password");
 
-passwordInput.addEventListener("pointerover", (e) => {
+  // Check if the password contains at least 1 small letter.
+  if (/[a-z]/.test(password))
+    document.getElementById("small").style.color = "green";
+  else {
+    document.getElementById("small").style.color = "grey";
+    numberOfConditions--;
+  }
+  // Check if the password contains at least 1 number.
+  if (/[0-9]/.test(password))
+    document.getElementById("number").style.color = "green";
+  else {
+    document.getElementById("number").style.color = "grey";
+    numberOfConditions--;
+  }
+  // Check if the password contains at least 1 special character.
+  if (/[!@#$%^&*()_+-]/.test(password))
+    document.getElementById("special").style.color = "green";
+  else {
+    document.getElementById("special").style.color = "grey";
+    numberOfConditions--;
+  }
+  if (password.length >= 8)
+    document.getElementById("length").style.color = "green";
+  else {
+    document.getElementById("length").style.color = "grey";
+    numberOfConditions--;
+  }
+  if (password == passCofirm) {
+    document.getElementById("confirm").style.color = "green";
+  } else {
+    document.getElementById("confirm").style.color = "grey";
+    numberOfConditions--;
+  }
+
+  
+  if (numberOfConditions!=6) {
+    document.getElementById("submetbtn").type = "button";
+  }
+  else
+  document.getElementById("submetbtn").type = "submet";
+
+}
+
+// Add an event listener to the password input fields
+const passwordInput = document.getElementById("password");
+const passwordConfirm = document.getElementById("passwordConfirm");
+
+passwordInput.addEventListener("keyup", (e) => {
   // Get the password value
-  const password = passwordInput.value;
-  checkPassword(document.querySelector("#password").value);
+  checkPassword(passwordInput.value, passwordConfirm.value);
 });
-// When the "Check Password" button is clicked, call the checkPassword() function.
+passwordConfirm.addEventListener("keyup", (e) => {
+  checkPassword(passwordInput.value, passwordConfirm.value);
+  
+});
